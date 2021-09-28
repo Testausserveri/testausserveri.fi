7c158efa1966e4ccf293f0dd6245446798ae8df7
Mikael <46541386+ahnl@users.noreply.github.com>
2021-09-28 12:01:42 +0300

    Fix FQDN

--- dns/dnsconfig.js
+++ dns/dnsconfig.js
@@ -57,10 +57,10 @@ function Mailerlite() {
 
 function Google() {
     return [
-        MX('@', 5, 'ALT1.ASPMX.L.GOOGLE.COM'),
-        MX('@', 5, 'ALT2.ASPMX.L.GOOGLE.COM'),
-        MX('@', 10, 'ALT3.ASPMX.L.GOOGLE.COM'),
-        MX('@', 10, 'ALT4.ASPMX.L.GOOGLE.COM'),
+        MX('@', 5, 'ALT1.ASPMX.L.GOOGLE.COM.'),
+        MX('@', 5, 'ALT2.ASPMX.L.GOOGLE.COM.'),
+        MX('@', 10, 'ALT3.ASPMX.L.GOOGLE.COM.'),
+        MX('@', 10, 'ALT4.ASPMX.L.GOOGLE.COM.'),
     ]
 }
 function Yandex() {
