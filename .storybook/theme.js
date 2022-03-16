ad14d70a92bee92c17cfe3d0c6d2283c83e8e878
Mikael <46541386+ahnl@users.noreply.github.com>
2022-03-16 13:32:07 +0200

    Add Storybook theming

---
+++ .storybook/theme.js
@@ -0,0 +1,8 @@
+import { create } from '@storybook/theming';
+
+export default create({
+  base: 'dark',
+  brandTitle: 'Testausserveri Coal',
+  brandUrl: 'https://testausserveri.fi',
+  brandImage: 'https://static.testausserveri.fi/testausserveri_logo_white_with_text.svg',
+});
