fefa75779d4631144c7fd54bc3809ac40e629bb9
developerfromjokela <developerfromjokela@gmail.com>
2022-04-05 00:31:57 +0300

    Patch: Ebin XSS injectons :DDDDDDD

--- role.php
+++ role.php
@@ -26,16 +26,33 @@ $response = json_decode($response, true);
 $name = $response['name'];
 $members = $response["members"];
 
+
 function remasterBio($bio) {
-    $bio = preg_replace('/<(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})>/', '<a href="$1">$1</a>', $bio);
-    $bio = str_replace(array("\r\n","\r","\n","\\r","\\n","\\r\\n"),"<br/>", $bio);
+    $urlRegex = '/<(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})>/';
+    $bio = str_replace(array("\r\n","\r","\n","\\r","\\n","\\r\\n"),"</br>", $bio);
     $bio = preg_replace('/\*\*([^"]*)\*\*/', '<b>$1</b>', $bio);
     $bio = preg_replace('/\*([^"]*)\*/', '<i>$1</i>', $bio);
+    $hyperlinks = array();
+    try {
+        preg_match_all($urlRegex, $bio, $urlRegexResult, PREG_SET_ORDER);
+        if (is_array($urlRegexResult) && count($urlRegexResult) > 0) {
+            foreach ($urlRegexResult as $hyperlink){
+                $id = "hyperlink". mt_rand(100000,999999);
+                $bio = str_replace('<'.$hyperlink[1].'>', $id, $bio);
+                $hyperlinks[$id] = $hyperlink[1];
+            }
+        }
+    } catch (Exception $ignored) {}
+    $bio = html_escape($bio);
+    foreach ($hyperlinks as $linkKey => $link) {
+        $bio = str_replace($linkKey, '<a href="'.htmlentities($link).'">'.htmlentities($link).'</a>', $bio);
+    }
     return $bio;
 }
 
-function filterText($text) {
-    return  preg_replace('/[[:^print:]]/', '', $text);
+function html_escape($text) {
+    $string = htmlentities($text);
+    return str_replace(array("&lt;i&gt;", "&lt;b&gt;", "&lt;/i&gt;", "&lt;/b&gt;", "&lt;/br&gt;"), array("<i>", "<b>", "</i>", "</b>", "</br>"), $string);
 }
 
 foreach ($members as $key => $member) {
@@ -92,20 +109,20 @@ function getAccountLink($account) {
     </tr>
     <?php foreach($members as $member): ?>
         <tr>
-            <th><img width="80" style="margin-left: 30px;margin-right: 30px;" src="<?php echo "img.php?url=".$member['avatar']?>"/> <br> <?php echo $member['displayName']?><br><?php echo $member['name']?>#<?php echo $member['discriminator']?></th>
-            <th><i><?php echo filterText($member['bio'])?></i></th>
+            <th><img width="80" style="margin-left: 30px;margin-right: 30px;" src="<?php echo "img.php?url=".urlencode($member['avatar'])?>"/> <br> <?php echo htmlentities($member['displayName'])?><br><?php echo htmlentities($member['name'])?>#<?php echo $member['discriminator']?></th>
+            <th><i><?php echo $member['bio']?></i></th>
             <th>
                 <center>
                 <table>
                 <?php foreach($member['presence'] as $memberActivity): ?>
-                <?php $imgUrl = ((array_key_exists("emoji", $memberActivity) && $memberActivity['emoji'] != null) ? $memberActivity['emoji']['url'] : ($memberActivity['assets']['smallImage'] != null ? $memberActivity['assets']['smallImage'] : $memberActivity['assets']['largeImage']))?>
+                <?php $imgUrl = urlencode((array_key_exists("emoji", $memberActivity) && $memberActivity['emoji'] != null) ? $memberActivity['emoji']['url'] : ($memberActivity['assets']['smallImage'] != null ? $memberActivity['assets']['smallImage'] : $memberActivity['assets']['largeImage']))?>
                     <tr>
                         <?php if ($imgUrl != null):?>
-                        <th><img width="30" style="margin-left: 30px;margin-right: 30px;" src="<?php echo "img.php?url=".$imgUrl?>"/></th>
+                        <th><img width="30" style="margin-left: 30px;margin-right: 30px;" src="<?php echo "img.php?url=".urlencode($imgUrl)?>"/></th>
                         <?php endif;?>
                         <th>
-                            <p><?php echo $memberActivity['name'];?>: <?php echo $memberActivity['state'];?></p>
-                            <p><?php echo $memberActivity['details'];?></p>
+                            <p><?php echo html_escape($memberActivity['name']);?>: <?php echo html_escape($memberActivity['state']);?></p>
+                            <p><?php echo html_escape($memberActivity['details']);?></p>
                         </th>
                     </tr>
                 <?php endforeach; ?>
@@ -117,7 +134,7 @@ function getAccountLink($account) {
             </th>
             <th>
                 <?php foreach($member['connectedAccounts'] as $account): ?>
-                <a href="<?php echo getAccountLink($account)?>"><?php echo $account['type']?>: <?php echo $account['name']?></a>
+                <a href="<?php echo getAccountLink($account)?>"><?php echo $account['type']?>: <?php echo html_escape($account['name'])?></a>
                 <br><hr>
                 <?php endforeach;?>
             </th>
