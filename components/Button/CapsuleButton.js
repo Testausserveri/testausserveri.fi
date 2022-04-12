e7149367e9491a16a1706151f0781f5a5eb0dce1
Mikael <46541386+ahnl@users.noreply.github.com>
2022-04-12 12:36:39 +0300

    Make button ripples work better on mobile

--- components/Button/CapsuleButton.js
+++ components/Button/CapsuleButton.js
@@ -19,6 +19,13 @@ export function CapsuleButton({style, children, variant, className}) {
         ripple.current.style.top = `${e.clientY - rect.top - 50}px`;
         ripple.current.style.left = `${e.clientX - rect.left - 50}px`;
     }
+    function onTouchMove(e) {
+        let rect = e.target.getBoundingClientRect()
+        let touch = e.touches[0]
+        ripple.current.style.top = `${touch.clientY - rect.top - 50}px`;
+        ripple.current.style.left = `${touch.clientX - rect.left - 50}px`;
+    }
+
     return (
         <div className={className} style={style}>
             <button 
@@ -26,6 +33,12 @@ export function CapsuleButton({style, children, variant, className}) {
                 onMouseEnter={() => setSize(1)} 
                 onMouseDown={() => setSize(1.2)} 
                 onMouseUp={() => setSize(1)} 
+
+                onTouchStart={() => setSize(1)}
+                onTouchEnd={() => setSize(0)}
+                onTouchMove={onTouchMove}
+                onClick={() => setSize(0)}
+
                 onMouseMove={onMouseMove} 
                 className={(variant === "small" ? `${styles.capsuleButton} ${styles.small}` : styles.capsuleButton)}>
                 {children}
