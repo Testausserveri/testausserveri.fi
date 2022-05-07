1f41c2c9c3049244ae3f5e9bf8e73f5cd300aefd
Eldemarkki <38920928+Eldemarkki@users.noreply.github.com>
2022-05-07 21:43:04 +0300

    Fix a few Storybook components (#40)
    * Make footer images white, fixes #37
    
    * Fix Navigation and Header stories
    
    * Fix Content story
    
    * Fix Testausserveri logo in Footer
    
    * Add .src back

--- components/Content/Content.stories.js
+++ components/Content/Content.stories.js
@@ -8,4 +8,8 @@ export default {
 const Template = (args) => <Content {...args} />
 
 export const Default = {
+  args: {
+    children: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ultricies leo integer malesuada nunc vel risus commodo viverra. Bibendum arcu vitae elementum curabitur vitae nunc sed velit. Quis enim lobortis scelerisque fermentum dui faucibus in ornare. Lorem dolor sed viverra ipsum nunc. Amet consectetur adipiscing elit pellentesque habitant morbi tristique. Senectus et netus et malesuada fames ac turpis egestas. Consectetur lorem donec massa sapien faucibus et molestie. Nisl condimentum id venenatis a condimentum vitae sapien pellentesque. Fermentum leo vel orci porta. Mauris pharetra et ultrices neque. Elementum curabitur vitae nunc sed velit dignissim. Mi in nulla posuere sollicitudin aliquam ultrices. Adipiscing bibendum est ultricies integer quis auctor. Tristique nulla aliquet enim tortor. In aliquam sem fringilla ut morbi tincidunt. Sapien pellentesque habitant morbi tristique senectus et netus et. Senectus et netus et malesuada fames ac turpis. Ac felis donec et odio.",
+    noMargin: true
+  }
 }
