bfd27c36477f1752b84d3cf3b586fafaa6c18549
Mikael <46541386+ahnl@users.noreply.github.com>
2022-06-15 19:23:56 +0300

    fix: getGuildInfo api server

--- utils/api.js
+++ utils/api.js
@@ -2,7 +2,7 @@ export const apiServer = process.env.NEXT_PUBLIC_API_SERVER
 export const apiServerMedia = process.env.NEXT_PUBLIC_API_SERVER_MEDIA
 
 export async function getGuildInfo(guildInfoModel) {
-    const response = await fetch(`https://api.testausserveri.fi/v1/discord/guildInfo${guildInfoModel ? "?r=" + guildInfoModel.join(",") : ""}`)
+    const response = await fetch(`${apiServer}/v1/discord/guildInfo${guildInfoModel ? "?r=" + guildInfoModel.join(",") : ""}`)
     const data = await response.json()
     return data
 }
