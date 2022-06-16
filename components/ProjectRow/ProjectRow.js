492298ef6e153da9a6ad0b271c92e106ce9881b5
Mikael <46541386+ahnl@users.noreply.github.com>
2022-06-16 21:25:54 +0300

    fix: make ProjectRow actually a link with correct behaviour

--- components/ProjectRow/ProjectRow.js
+++ components/ProjectRow/ProjectRow.js
@@ -7,23 +7,25 @@ import styles from './ProjectRow.module.css'
 export function ProjectRow({project}) {
     return (
         <Link href={`/projects/${project.slug}`}>
-            <div className={styles.row}>
-                <div className={styles.inner}>
-                    <div className={styles.image}>
-                        <img src={project.cover.url} />
-                    </div>
-                    <div className={styles.content}>
-                        <h2>{project.name}</h2>
-                        <p>
-                            {project.description.short}
-                        </p>
-                        <div className={styles.bottom}>
-                            <AvatarRow members={project.members} />
-                            <TagsRow tags={project.tags} />
+            <a>
+                <div className={styles.row}>
+                    <div className={styles.inner}>
+                        <div className={styles.image}>
+                            <img src={project.cover.url} />
+                        </div>
+                        <div className={styles.content}>
+                            <h2>{project.name}</h2>
+                            <p>
+                                {project.description.short}
+                            </p>
+                            <div className={styles.bottom}>
+                                <AvatarRow members={project.members} />
+                                <TagsRow tags={project.tags} />
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
+            </a>
         </Link>
     )
 }
