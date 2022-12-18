db0374c7817b2abed6f3ce544042d8c38e862079
Esinko <34211401+Esinko@users.noreply.github.com>
2022-12-18 14:19:13 +0200

    feat: login logic for members page

--- components/Login/Login.js
+++ components/Login/Login.js
@@ -9,6 +9,7 @@ const DynamicTestausid = dynamic(() => import('@testausserveri/react-testausid')
 })
 
 import FadeIn from 'react-fade-in';
+import { apiServer } from '../../utils/api';
 
 export function LoginDialog({ onClose }) {
     const accept = [
@@ -16,12 +17,36 @@ export function LoginDialog({ onClose }) {
       'discord'
     ]
     const client = '181937620043556561658238287560538816854'
-    const scopes = ['id', 'account']
+    const scopes = ['id', 'account', 'contact']
   
-    return <DynamicTestausid onClose={onClose} accept={accept} client={client} scopes={scopes} onClick={e => {
-      e.preventDefault()
-      e.stopPropagation()
-    }} />  
+    return <DynamicTestausid
+      onClose={onClose}
+      accept={accept}
+      client={client}
+      scopes={scopes}
+      onClick={e => {
+        e.preventDefault()
+        e.stopPropagation()
+      }}
+      onlyToken={true}
+      onLogin={token => {
+        fetch(`${apiServer}/v1/members/login`, {
+          method: "POST",
+          body: JSON.stringify(token),
+          redirect: "manual",
+          headers: {
+            "Content-Type": "application/json"
+          }
+        }).then(async res => {
+          if (res.status === 200) {
+            if (document.cookie.includes("code=")) document.cookie = document.cookie.replace(/code=(.{1,}|);( |)domain=\.testausserveri\.fi;( |)path=\/(;|$)/, "")
+            document.cookie += `code=${await res.text()};domain=.testausserveri.fi;path=/;`
+            window.location.href = `${apiServer}/v1/members`
+          }
+          else console.error("Failed to login.") // TODO: display to user
+        })
+      }}
+    />  
 }
   
 export function LoginView({visible, setLoginVisible}) {
