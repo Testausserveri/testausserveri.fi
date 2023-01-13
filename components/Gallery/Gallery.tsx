e54217e3a427d9fdab37f2c1222f536161dfd49c
Eldemarkki <38920928+Eldemarkki@users.noreply.github.com>
2023-01-13 17:11:59 +0200

    chore(typescript): migrate Gallery to typescript

--- components/Gallery/Gallery.tsx
+++ components/Gallery/Gallery.tsx
@@ -2,9 +2,15 @@ import Image from 'next/image'
 import { useEffect, useState } from 'react'
 import styles from './Gallery.module.css'
 
+export type GalleryProps = {
+    media: {
+        cover?: boolean,
+        url?: string
+    }[]
+}
 
-export function Gallery({media}) {
-    const [ activeMedia, setActiveMedia ] = useState(0)
+export function Gallery({ media }: GalleryProps) {
+    const [activeMedia, setActiveMedia] = useState(0)
     const items = media.sort((a, b) => (a.cover ? -1 : 0))
 
     useEffect(() => {
@@ -14,9 +20,9 @@ export function Gallery({media}) {
     return (
         <div>
             <div className={styles.display}>
-                {items[activeMedia]?.url ? 
+                {items[activeMedia]?.url ?
                     <Image src={items[activeMedia].url} layout="fill" />
-                : null}
+                    : null}
             </div>
             <ul className={styles.chooser}>
                 {items.map((item, i) => (
