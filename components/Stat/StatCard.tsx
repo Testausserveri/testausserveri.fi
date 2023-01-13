820458aca28c62dfdba0494b747290bb1a2b8259
Eldemarkki <38920928+Eldemarkki@users.noreply.github.com>
2023-01-13 17:25:01 +0200

    chore(typescript): migrate StatCard to typescript

--- components/Stat/StatCard.tsx
+++ components/Stat/StatCard.tsx
@@ -1,6 +1,12 @@
+import { ReactNode } from 'react'
 import styles from './StatCard.module.css'
 
-export function StatCard({label, value}) {
+export type StatCardProps = {
+    label: ReactNode,
+    value: ReactNode
+}
+
+export function StatCard({ label, value }: StatCardProps) {
     return (
         <div className={styles.statCard}>
             <span className={styles.label}>{label}</span>
