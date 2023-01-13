50c4aade080d2fdb8415dfd8f352c06946d00f8c
Eldemarkki <38920928+Eldemarkki@users.noreply.github.com>
2023-01-13 17:13:05 +0200

    chore(typescript): migrate GradientText to typescript

--- components/GradientText/GradientText.js
+++
@@ -1,9 +0,0 @@
-import styles from './GradientText.module.css'
-
-export function GradientText(props) {
-    return (
-        <span {...props} className={props.className ? `${styles.gradientText} ${props.className}` : styles.gradientText}>
-            {props.children}
-        </span>
-    )
-}
