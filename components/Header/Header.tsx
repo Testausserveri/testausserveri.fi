9caa757e12106e8d032dd1e3c6cfc25f6b7fa41f
Eldemarkki <38920928+Eldemarkki@users.noreply.github.com>
2023-03-18 01:07:24 +0200

    chore: remove login button, view and page to allow for upgrade to React 18 and Next.js 13

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -4,8 +4,6 @@ import { Navigation } from '../Navigation/Navigation'
 import styles from './Header.module.css'
 import DiscordIcon from '../../assets/DiscordIcon.svg'
 import { useState } from 'react'
-import { IoMdKey } from "react-icons/io"
-import { LoginView } from '../Login/Login'
 
 export type HeaderProps = {
     pages: {
@@ -17,20 +15,12 @@ export type HeaderProps = {
 
 export function Header({ pages, activePath }: HeaderProps) {
     const [open, setOpen] = useState(false)
-    const [loginVisible, setLoginVisible] = useState(false)
 
     return (
         <div className={`${styles.header} ${open ? styles.open : ""}`}>
-            <LoginView visible={loginVisible} setLoginVisible={setLoginVisible} />
             <Logo className={styles.logo} showBeta link />
             <Navigation className={styles.navigation} pages={pages} activePath={activePath} open={open} setOpen={setOpen} />
             <a className={styles.navButtons}>
-                <a onClick={() => setLoginVisible(true)} style={{ display: process.env.NODE_ENV != "production" ? "block" : "none" }}>
-                    <CapsuleButton className={styles.button} small secondary>
-                        <IoMdKey />
-                        Jäsensivut
-                    </CapsuleButton>
-                </a>
                 <a href="https://discord.testausserveri.fi">
                     <CapsuleButton className={styles.button} small>
                         <ButtonIcon src={DiscordIcon} />
