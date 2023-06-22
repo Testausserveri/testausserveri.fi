e723ad6a27254434eab03cfcc51d32452461c3b8
Mikael <46541386+ahnl@users.noreply.github.com>
2023-06-22 21:49:13 +0300

    fix: tooltip text style

--- components/Explanation/Explanation.tsx
+++ components/Explanation/Explanation.tsx
@@ -11,7 +11,7 @@ export function Explanation({ children }: ExplanationProps) {
 
     return (
         <>
-            <Tooltip id={id} style={{maxWidth: "300px"}}>
+            <Tooltip id={id} style={{maxWidth: "300px"}} className="tip">
                 {children}
             </Tooltip>
             <span className={styles.explanation} data-tooltip-id={id}><RiQuestionLine /></span>
