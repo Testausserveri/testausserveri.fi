c7c3ce2fae609ad4dc873743f65c0fe94fb76bee
Mikael <46541386+ahnl@users.noreply.github.com>
2024-03-01 23:18:48 +0200

    fix: default to prod in dev

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 const { PHASE_DEVELOPMENT_SERVER } = require('next/constants')
 
 // The lines API_SERVER_DEV and URL_DEV are the most significant to you
-const API_SERVER_DEV = `http://localhost:8081`
+const API_SERVER_DEV = `https://api.testausserveri.fi`
 const API_SERVER_PROD = `https://api.testausserveri.fi`
 
-const URL_DEV = `http://localhost:3001`
+const URL_DEV = `http://localhost:3000`
 const URL_PROD = `https://testausserveri.fi`
 
 const MEDIA_API_SERVER = 'https://api.testausserveri.fi'
