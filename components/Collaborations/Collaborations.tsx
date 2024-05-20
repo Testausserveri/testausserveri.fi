df4f29ac55074067221c19fe4e8689eb9cb00c1f
Antti Ellilä <antti@antti.codes>
2024-05-20 13:21:01 +0300

    revert: "fix: Remove misplaced logo"
    Permission has now been granted
    
    This reverts commit 13ed068ae73d76a3ed26f109748fe5be8b93839f.

--- components/Collaborations/Collaborations.tsx
+++ components/Collaborations/Collaborations.tsx
@@ -43,6 +43,11 @@ export function Collaborations(props: CollaborationsProps) {
                     </Link>
                 </li>
                 <li>
+                    <Link href="https://poliisi.fi/cybercrime-exit" target="_blank">
+                        <Image src={krp} height={80} alt="KRP logo" />
+                    </Link>
+                </li>
+                <li>
                     <Link href="https://koodiasuomesta.fi/" target="_blank">
                         <Image src={koodiasuomesta} height={80} alt="Koodia Suomesta logo" />
                     </Link>
