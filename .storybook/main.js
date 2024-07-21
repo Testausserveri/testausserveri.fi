644c484a65e6db92ad4212fdc2b358916ce2bc35
Mikael <46541386+ahnl@users.noreply.github.com>
2024-07-21 13:28:20 +0300

    chore: upgrade next, node, and other deps

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,12 +2,19 @@ module.exports = {
   "stories": [
     "../components/**/*.stories.@(js|jsx|ts|tsx)"
   ],
+
   "addons": [
-    "@storybook/addon-essentials", 
-    "storybook-css-modules-preset"
+    "@storybook/addon-essentials",
+    "storybook-css-modules-preset",
+    "@chromatic-com/storybook"
   ],
+
   "framework": {
     name: "@storybook/nextjs",
     options: {}
   },
+
+  typescript: {
+    reactDocgen: "react-docgen-typescript"
+  }
 };
