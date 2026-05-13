cc3b62c04d38f742f49536afb3cced1ada91309a
Mikael Hannolainen <46541386+ahnl@users.noreply.github.com>
2026-05-13 20:26:09 +0300

    fix: correct tapahtumakalenteri link :shipit:

--- next.config.js
+++ next.config.js
@@ -63,7 +63,7 @@ const config = {
       { "source": "/link/:path*", "destination": "https://link.testausserveri.fi/:path*", permanent: true },
       { "source": "/yhdistyksen-saannot.pdf", "destination": "/association-rules", permanent: true },
       { "source": "/host/privacy", "destination": "/privacy/host", permanent: true },
-	  { "source": "/tapahtumakalenteri.ics", "destination": "https://calendar.google.com/calendar/u/0?cid=Y18ybjF0Y2cyaDM1YmZwZTI0MjFxdG9tZGloNEBncm91cC5jYWxlbmRhci5nb29nbGUuY29t", permanent: false }
+	  { "source": "/tapahtumakalenteri.ics", "destination": "https://calendar.google.com/calendar/ical/c_2n1tcg2h35bfpe2421qtomdih4%40group.calendar.google.com/public/basic.ics", permanent: false }
     ];
   },
   webpack: (config, options) => {
